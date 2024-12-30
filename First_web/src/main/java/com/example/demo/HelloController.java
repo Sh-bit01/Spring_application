d652d56fca7712c8e60b1077b63bfbe38057cc5c
Marlinspike <shreyashgondane099@gmail.com>
2024-12-31 05:13:42 +0530

    calc web apk commit

--- First_web/src/main/java/com/example/demo/HelloController.java
+++ First_web/src/main/java/com/example/demo/HelloController.java
@@ -3,15 +3,53 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller // Marks this class as a controller for handling REST API requests
 public class HelloController {
 
-    @GetMapping("/hii")  // Maps to /hello URL
+    @GetMapping("/calc")  // Maps to /hello URL
     public String sayHello(Model model) {
-            model.addAttribute("message", "Hello, Spring Boot!");
-
+         //model.addAttribute("message", "Hello, Spring Boot!");
         return "user";
+    }
+    
+    @GetMapping("/add")
+    public String add(@RequestParam double num1, @RequestParam double num2, Model model) {
+        double result = num1 + num2;
+        
+        return "The sum of " + num1 + " and " + num2 + " is " + result;
+    }
+
+    // Example: POST /mul
+    @PostMapping("/mul")
+    public String mul(@RequestBody Data request, Model model) {
+        double num1 = Double.parseDouble(request.getNum1());
+        double num2 = Double.parseDouble(request.getNum2());
+        double res = num1 * num2;
+    	model.addAttribute("message", "Mul");    	    	
+    	model.addAttribute("res", res);    	
+        return "result";
+    }
+    
+    // Example: POST /sub
+    @PostMapping(value = "/sub", consumes = "application/x-www-form-urlencoded")
+    public String sub(@RequestBody Data request, Model model) {
+        double res = Double.parseDouble(request.getNum1()) - Double.parseDouble(request.getNum2());
+    model.addAttribute("message", "Subtraction Result");
+    model.addAttribute("res", res);
+    return "result";
+    }
+    // Example: POST /div
+    @PostMapping("/div")
+    public String div(@RequestBody Data request, Model model) {
+
+        double res = Double.parseDouble(request.getNum1()) / Double.parseDouble(request.getNum2());
+    	model.addAttribute("message", "div");    	    	
+    	model.addAttribute("res", res);    	
+        return "result";
     }
 }
 
